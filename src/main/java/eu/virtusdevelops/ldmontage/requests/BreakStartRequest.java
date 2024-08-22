64acc79595ac6e48434916eb55888fe13a41a55c
Gasper Lukman <gaspercino.lukman@gmail.com>
2024-08-22 14:38:40 +0200

    Some constraints

--- src/main/java/eu/virtusdevelops/ldmontage/requests/BreakStartRequest.java
+++ src/main/java/eu/virtusdevelops/ldmontage/requests/BreakStartRequest.java
@@ -1,7 +1,16 @@
 package eu.virtusdevelops.ldmontage.requests;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 public record BreakStartRequest(
+
+        @Max(value = 180, message = "Longitude must be between -180 and 180 degrees!")
+        @Min(value = -180, message = "Longitude must be between -180 and 180 degrees!")
         double longitude,
+
+        @Max(value = 90, message = "Latitude must be between -90 and 90 degrees!")
+        @Min(value = -90, message = "Latitude must be between -90 and 90 degrees!")
         double latitude
         // break type
 
--- src/main/java/eu/virtusdevelops/ldmontage/requests/WorkSiteCreateRequest.java
+++ src/main/java/eu/virtusdevelops/ldmontage/requests/WorkSiteCreateRequest.java
@@ -1,10 +1,23 @@
 package eu.virtusdevelops.ldmontage.requests;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
 public record WorkSiteCreateRequest(
+        @NotEmpty(message = "Work title can not be empty")
         String title,
+
         String description,
-        double latitude,
+
+        @Max(value = 180, message = "Longitude must be between -180 and 180 degrees!")
+        @Min(value = -180, message = "Longitude must be between -180 and 180 degrees!")
         double longitude,
+
+        @Max(value = 90, message = "Latitude must be between -90 and 90 degrees!")
+        @Min(value = -90, message = "Latitude must be between -90 and 90 degrees!")
+        double latitude,
+
         long workId
 ) {
 }
